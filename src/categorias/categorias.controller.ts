f6a2370e4c6d243ccd9f0490da43f01941129b88
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2021-04-21 00:13:47 -0300

    feat: getAll getById de categorias

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
 import { CriarCategoriaDTO } from './dtos/criar-categoria.dto';
 import { Categoria } from './interfaces/categoria.interface';
@@ -16,6 +16,18 @@ export class CategoriasController {
     @Body()criarCategoriaDTO: CriarCategoriaDTO
   ): Promise<Categoria> {
     return await this.categoriasService.criarCategoria(criarCategoriaDTO)
+  }
+
+  @Get()
+  async consultarCategorias(): Promise<Categoria[]> {
+    return await this.categoriasService.consultarTodasCategorias()
+  }
+
+  @Get('/:categoria')
+  async consultarCategoriaPeloId(
+    @Param('categoria') categoria: string
+  ): Promise<Categoria> {
+    return await this.categoriasService.consultarCategoriaPeloId(categoria)
   }
 
 }
--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CriarCategoriaDTO } from './dtos/criar-categoria.dto';
@@ -23,6 +23,20 @@ export class CategoriasService {
     const categoriaCriada = new this.categoriaModel(criarCategoriaDTO);
     return await categoriaCriada.save();
 
+  }
+
+  async consultarTodasCategorias(): Promise<Categoria[]> {
+    return await this.categoriaModel.find().exec();
+  }
+
+  async consultarCategoriaPeloId(categoria: string) {
+    const categoriaEncontrada = await this.categoriaModel.findOne({ categoria }).exec()
+
+    if(!categoriaEncontrada) {
+      throw new NotFoundException(`Categoria ${categoria} não encontrada!`)
+    }
+
+    return categoriaEncontrada
   }
 
 }
