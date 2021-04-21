7ce596fe06ce0faef9287ed8407657819d2eb872
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2021-04-21 13:49:24 -0300

    feat: implementação do filter http-exception

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -3,7 +3,7 @@ import { AtualizarJogadorDTO } from './dtos/atualizar-jogador.dto';
 import { CriarJogadorDTO } from './dtos/criar-jogador.dto'
 import { Jogador } from './interfaces/jogador.interface';
 import { JogadoresService } from './jogadores.service';
-import { JogadoresValidacaoParametrosPipe } from './pipes/jogadores-validacao-parametros.pipe';
+import { ValidacaoParametrosPipe } from '../common/pipes/validacao-parametros.pipe';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
@@ -22,7 +22,7 @@ export class JogadoresController {
   @UsePipes(ValidationPipe)
   async atualizarJogador(
     @Body() atualizarJogadorDTO: AtualizarJogadorDTO,
-    @Param('_id', JogadoresValidacaoParametrosPipe) _id: string
+    @Param('_id', ValidacaoParametrosPipe) _id: string
   ): Promise<void> {
     await this.jogadoresService.atualizarJogador(_id, atualizarJogadorDTO);
   }
@@ -34,14 +34,14 @@ export class JogadoresController {
 
   @Get('/:_id')
   async consultarJogadorPeloId(
-    @Param('_id', JogadoresValidacaoParametrosPipe) _id: string
+    @Param('_id', ValidacaoParametrosPipe) _id: string
   ): Promise<Jogador> {
     return this.jogadoresService.consultarJogadorPeloId(_id);
   }
 
   @Delete('/:_id')
   async deletarJogador(
-    @Param('_id', JogadoresValidacaoParametrosPipe) _id: string
+    @Param('_id', ValidacaoParametrosPipe) _id: string
   ): Promise<void> {
     this.jogadoresService.deletarJogadorPorEmail(_id);
   }
